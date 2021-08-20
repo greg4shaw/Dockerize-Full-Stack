a29b73934a785c5d6481ae08d0a1b0666ae135e0
greg <gman.international@gmail.com>
2021-08-20 19:04:01 +0200

    2021_08_19_v1

--- server/src/database.js
+++ server/src/database.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const User = require('./models/user.model');
 
-const connection = 'mongodb://localhost:27017/dockerize-me';
+//const connection = 'mongodb://localhost:27017/dockerize-me';
+const connection = 'mongodb://mongo:27018/dockerize-me'; 
+
 
 const connectDb = () => {
   return mongoose.connect(connection);
